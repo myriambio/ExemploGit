426f71e86c184b98f1f0fd82fc715d3cf3ca693b
Myriam Martins <myriambio@yahoo.com.br>
2018-05-23 20:06:43 -0300

    add uma pasta lista

---
+++ Lista/src/lista/Lista.java
@@ -0,0 +1,16 @@
+package lista;
+
+public class Lista {
+	
+	public No inicio;
+	
+	public void inserirInicio (int c) {
+		No aux = new No(c);
+	}
+	
+	
+	public void listar () {
+		
+	}
+
+}
---
+++ Lista/src/lista/No.java
@@ -0,0 +1,15 @@
+package lista;
+
+public class No {
+	
+	public No prox;
+	public int conteudo;
+
+	public No (int C) {
+		conteudo=C;
+		prox=null;
+	}
+}
+
+
+
